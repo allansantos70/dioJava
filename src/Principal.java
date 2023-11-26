7da5d6895ff3d3c9c138e872a2ab5235041d37c9
Alan Santos <allanbatista70@outlook.com>
2023-11-25 21:44:33 -0300

    dois

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Principal {
     
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        
+
     }
 }
